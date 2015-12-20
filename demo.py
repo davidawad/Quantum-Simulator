69595336353d2a4ba7747740be1a1fcb3ca829fb
David Awad <davidawad64@gmail.com>
2015-12-19 21:51:16 -0500

    barebones demo

---
+++ demo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import quantum
+
+if __name__ == "__main__":
+
+    # set up a system with 3 qubits
+    psi = quantum.Psi(3)
+
+    print(psi.amplitudes)
+
+    psi.pi_over_eight(5)
+
+#    print()
+
+    # apply Hadamard gate to qubit 1 (second qubit)
+    # psi.hadamard(1)
+
+    # apply Hadamard gate to qubit 2 (third qubit)
+    # psi.hadamard(2)
+
+    # print(psi.amplitudes)
+
+    # apply π/8 gate to qubit 1
+    # psi.pi_over_eight(1)
+
+    # apply controlled-not gate with qubit 1 controlling qubit 0
+    psi.controlled_not(1, 0)
+
+    # collapse and print the result (a tuple of 3 classical bits)
+    # print(psi.collapse())
