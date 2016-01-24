297d8471c098e213011ae1393d0ce167d2b28136
David Awad <davidawad64@gmail.com>
2016-01-24 03:10:06 -0500

    internal dict representation

--- quantum.py
+++ quantum.py
@@ -3,9 +3,13 @@
 
 from cmath import exp, pi, sqrt
 from random import random
-
+import itertools
 
 class Psi:
+    """
+        This class represents the wave function describing a
+        basic quantum computer consisting of n qubits.
+    """
     def __init__(self, num_qubits):
         """
         set up a quantum system with the given number of qubits
@@ -15,7 +19,21 @@ class Psi:
         # In this classical simulation,we use 2^n Qubit complex numbers
         # this array of size 2^n will replicate the 2^n states Qubits can have
         self.amplitudes = [0] * (1 << num_qubits)  # use bitshift to realize 2^n
-        self.amplitudes[0] = 1  # so that sum of squared prob. is 1, starting in 000 state
+        self.amplitudes[0] = 1  # so that sum of prob.s is 1, starting in 000 state
+        self.states = self.generate_states()
+
+    def generate_states(self, num_qubits):
+        """
+        returns a dictionary of all possible states for n qubits
+        in the format { 'state': 'amplitude' } e.g. { 010 : 0j}
+        """
+        if num_qubits <= 0:
+            raise ValueError("simulation requires at least 1 qubit")
+        # generate table of states using itertools
+        tuples = [''.join(_) for _ in itertools.product(['0', '1'], repeat=num_qubits)]
+        data = {}
+        map(lambda x: data.update({x:0}), tuples)
+        return data
 
     def collapse(self):
         """
